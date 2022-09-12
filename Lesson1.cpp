189b6b6bd7f3d9406aa5fbe3584c5603f973ed20
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-12 10:10:59 +0200

    Add project files.

---
+++ Lesson1.cpp
@@ -0,0 +1,45 @@
+// Lesson1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c,d,e,f;
+    float g,h,i;
+    a = 7;
+    b = 5;
+    h = 7;
+    i = 5;
+    c = a + b;
+    d = a - b;
+    e = a * b;
+    f = a / b;
+    g = a / b;
+    printf("Add up: %d + %d = %d\n", a, b, c);
+    printf("Minus: %d - %d = %d\n", a, b, d);
+    printf("Multiply: %d * %d = %d\n", a, b, e);
+    printf("Divide: %d / %d = %d\n", a, b, f);
+    printf("Divide: %d / %d = %f\n", a, b, g);
+
+    //std::cout << "Hello World!\n";
+    std::cout << " Horny ";
+    std::cout << " The bee movie script: ";
+    
+    printf(" sdf ");
+    printf(" lkj ");
+    cout << " asd ";
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
