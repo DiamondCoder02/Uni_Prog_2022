5957516b5b0b361885b3b9722e7d083575ae30b3
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-15 19:40:06 +0200

    Homework1

--- Lesson1.cpp
+++ Lesson1.cpp
@@ -1,9 +1,143 @@
 // Lesson1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 using namespace std;
 
+//Homework1
+int main()
+{
+    float owo, nya, lewd, creamPi, horny;
+    string uwu; 
+    int objectNum;
+    bool dim;
+    owo = 0.0;
+    nya = 0.0;
+    lewd = 0.0;
+    horny = 0.0;
+    creamPi = 3.14159265359;
+
+    cout << "Do we calculate a 3d object? (1 for true or 0 for false)";
+    cin >> dim;
+    try {
+        if (dim) {
+            cout << "What's the object? \n1 Cube\n2 Cuboid\n3 Pyramid\n4 Sphere\n5 Cylinder";
+            cin >> objectNum;
+            cout << "What would you like to calculate? Surfece or Volume? (Type S or V) ";
+            cin >> uwu;
+            if (objectNum == 1) {
+                cout << "Please give me a side: ";
+                cin >> owo;
+                lewd = (owo * owo * 6);
+                printf("Cube Surface is %f \n", lewd);
+                lewd = (owo * owo * owo);
+                printf("Cube Volume is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 2) {
+                cout << "Please give me one side: ";
+                cin >> owo;
+                cout << "Please give me second side: ";
+                cin >> nya;
+                cout << "Please give me third side: ";
+                cin >> horny;
+                lewd = (2*(owo*nya+owo*horny+nya*horny));
+                printf("Cuboid Surface is %f \n", lewd);
+                lewd = (owo*nya*horny);
+                printf("Cuboid Volume is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 3) {
+                cout << "Please give me the base side: ";
+                cin >> owo;
+                cout << "Please give me the height: ";
+                cin >> nya;
+                cout << "Please give me the !! side area !! : ";
+                cin >> horny;
+                lewd = (owo * owo * horny);
+                printf("Pyramid Surface is %f \n", lewd);
+                lewd = (owo * owo * nya * 1/3);
+                printf("Pyramid Volume is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 4) {
+                cout << "Please give me the radius: ";
+                cin >> owo;
+                lewd = (owo * owo * creamPi * 4);
+                printf("Sphere Surface is %f \n", lewd);
+                lewd = (4 / 3 * creamPi * owo * owo * owo);
+                printf("Sphere Volume is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 5) {
+                cout << "Please give me the radius: ";
+                cin >> owo;
+                cout << "Please give me the height: ";
+                cin >> nya;
+                lewd = (owo * owo * creamPi * nya);
+                printf("Cylinder Surface is %f \n", lewd);
+                lewd = (2 * owo * owo * creamPi) + (2 * owo * creamPi * nya);
+                printf("Cylinder Volume is %f \n", lewd);
+                return 0;
+            }
+
+
+            
+        }
+        else {
+            cout << "What's the object? \n1 Square\n2 Rectangle\n3 Triangle\n4 Circle\n5 Ellipse";
+            cin >> objectNum;
+            if (objectNum == 1) {
+                cout << "Please give me the side: ";
+                cin >> owo;
+                lewd = (owo * owo);
+                printf("Rectangle Area is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 2) {
+                cout << "Please give me one side: ";
+                cin >> owo;
+                cout << "Please give me other side: ";
+                cin >> nya;
+                lewd = (owo * nya);
+                printf("Rectangle Area is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 3) {
+                cout << "Please give me the base: ";
+                cin >> owo;
+                cout << "Please give me the height: ";
+                cin >> nya;
+                lewd = ((owo * nya) /0.5 );
+                printf("Triangle Area is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 4) {
+                cout << "Please give me the radius: ";
+                cin >> owo;
+                lewd = (owo * owo * creamPi);
+                printf("Circle Area is %f \n", lewd);
+                return 0;
+            }
+            else if (objectNum == 5) {
+                cout << "Please give me one radius: ";
+                cin >> owo;
+                cout << "Please give me second radius: ";
+                cin >> nya;
+                lewd = (owo * creamPi * nya);
+                printf("Ellipse Area is %f \n", lewd);
+                return 0;
+            }
+        }
+    }
+    catch (...) {
+        printf("\nError, you made mistake! \n");
+        return 0;
+    }
+}
+
+
+// Lesson1
+/*
 int main()
 {
     int a, b, c,d,e,f;
@@ -32,14 +166,4 @@ int main()
     cout << " asd ";
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+*/
