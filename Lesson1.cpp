2745575ad36163542d273a3974b8a93adc13951a
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-19 08:36:43 +0200

    hw fix 2 (no more shit)

--- Lesson1.cpp
+++ Lesson1.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+//On old system: setlocale(LC_ALL, "hun");
 
 //Homework1
 int main()
@@ -29,7 +30,6 @@ int main()
                 printf("Cube Surface is %f \n", lewd);
                 lewd = (owo * owo * owo);
                 printf("Cube Volume is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 2) {
                 cout << "Please give me one side: ";
@@ -42,7 +42,6 @@ int main()
                 printf("Cuboid Surface is %f \n", lewd);
                 lewd = (owo*nya*horny);
                 printf("Cuboid Volume is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 3) {
                 cout << "Please give me the base side: ";
@@ -54,7 +53,6 @@ int main()
                 printf("Pyramid Surface is %f \n", lewd);
                 lewd = (owo * owo * nya * 1/3);
                 printf("Pyramid Volume is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 4) {
                 cout << "Please give me the radius: ";
@@ -63,7 +61,6 @@ int main()
                 printf("Sphere Surface is %f \n", lewd);
                 lewd = (4 / 3 * creamPi * owo * owo * owo);
                 printf("Sphere Volume is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 5) {
                 cout << "Please give me the radius: ";
@@ -74,22 +71,20 @@ int main()
                 printf("Cylinder Surface is %f \n", lewd);
                 lewd = (2 * owo * owo * creamPi) + (2 * owo * creamPi * nya);
                 printf("Cylinder Volume is %f \n", lewd);
-                return 0;
             }
             else {
                 printf("\n__Error, you made mistake!__ \n");
-                return 0;
             }
+            return 0;
         }
         else {
-            cout << "What's the object? \n1 Square\n2 Rectangle\n3 Triangle\n4 Circle\n5 Ellipse";
+            cout << "What's the object? \n1 Square\n2 Rectangle\n3 Triangle\n4 Circle\n5 Ellipse\nChoosen:";
             cin >> objectNum;
             if (objectNum == 1) {
                 cout << "Please give me the side: ";
                 cin >> owo;
                 lewd = (owo * owo);
                 printf("Rectangle Area is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 2) {
                 cout << "Please give me one side: ";
@@ -98,7 +93,6 @@ int main()
                 cin >> nya;
                 lewd = (owo * nya);
                 printf("Rectangle Area is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 3) {
                 cout << "Please give me the base: ";
@@ -107,14 +101,12 @@ int main()
                 cin >> nya;
                 lewd = ((owo * nya) /0.5 );
                 printf("Triangle Area is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 4) {
                 cout << "Please give me the radius: ";
                 cin >> owo;
                 lewd = (owo * owo * creamPi);
                 printf("Circle Area is %f \n", lewd);
-                return 0;
             }
             else if (objectNum == 5) {
                 cout << "Please give me one radius: ";
@@ -123,12 +115,11 @@ int main()
                 cin >> nya;
                 lewd = (owo * creamPi * nya);
                 printf("Ellipse Area is %f \n", lewd);
-                return 0;
             }
             else {
                 printf("\n__Error, you made mistake!__ \n");
-                return 0;
             }
+            return 0;
         }
     }
     catch (...) {
