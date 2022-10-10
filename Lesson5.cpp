4c6c25d7f48764730783b13a6e1e89a264a74a16
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-10-10 19:52:07 +0200

    lesson 5 done

--- Lesson5.cpp
+++ Lesson5.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <cmath>
 
+#define MAX 5
+
 using namespace std;
 //On old system: setlocale(LC_ALL, "hun");
 
@@ -39,7 +41,6 @@ int main() {
 			putchar(c);
 			c = getchar();
 		}
-		return 0;
 	}
 	else if (exercise == 3) {
 		int a, b, c;
@@ -76,7 +77,28 @@ int main() {
 		return 0;
 	}
 	else if (exercise == 5) {
-
+		int tomb[MAX];
+		int i, j;
+		//Tömb feltöltése
+		for (i = 0; i < MAX; i++) { printf("Kerem a %d. szamot ", i + 1); scanf_s("%d", &tomb[i]); }
+		//Minimum- és maximumkeresés
+		int min, max;
+		min = max = tomb[0];
+		for (i = 1; i < MAX; i++) {
+			if (tomb[i] > max)max = tomb[i];
+			if (tomb[i] < min)min = tomb[i];
+		}
+		printf("Minimum = %d\nMaximun = %d\n", min, max);
+		//Buborékrendezés
+		int csere;
+		for (j = 0; j < MAX - 1; j++)
+			for (i = 0; i < MAX - 1 - j; i++)
+				if (tomb[i] > tomb[i + 1]) {
+					csere = tomb[i];
+					tomb[i] = tomb[i + 1];
+					tomb[i + 1] = csere;
+				}
+		for (i = 0; i < MAX; i++) printf("%d\n", tomb[i]);
 		return 0;
 	}
 	else {
